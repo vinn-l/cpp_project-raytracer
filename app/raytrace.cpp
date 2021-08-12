1153f456e53bbefda0953ea2f2ea5751c0239bb5
waltvin <waltvin.l@gmail.com>
2021-08-12 13:49:55 -0700

    initial commit, simple ppm image creation

---
+++ app/raytrace.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+int main() {
+
+    // Image Properties
+
+    const int image_width = 256;
+    const int image_height = 256;
+
+    // Create PPM Image
+    // Apparently Windows machine creates PPM file with UTF-16 encoding, and GIMP is unable to open this without changing the PPM encoding to ASCII.
+
+    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+    for (int j = 0; j < image_height; j++) {
+        for (int i = 0; i < image_width; ++i) {
+            auto r = double(i) / (image_width-1);
+            auto g = double(j) / (image_height-1);
+            auto b = 0.25;
+
+            int ir = static_cast<int>(255.999 * r);
+            int ig = static_cast<int>(255.999 * g);
+            int ib = static_cast<int>(255.999 * b);
+
+            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+        }
+    }
+
+    return 0
+}
